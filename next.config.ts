5622479b0115bbbf9aeb0a4a772cfc73c81abfc4
omejeemmanuel1 <omejeemanuel046@yahoo.com>
2026-02-07 00:14:40 +0100

    feat: add /download rewrite to backend

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,21 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-module.exports = {
   images: {
-    qualities: [100, 25, 50, 75],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "backend-api.helpappafrica.com",
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/download',
+        destination: 'https://backend-api.helpappafrica.com/download',
+      },
+    ];
   },
 };
 
